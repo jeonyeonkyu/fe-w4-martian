145af456ac954273d2e1ee40fd4a3b1261bf7308
jeonyeonkyu <ehb6915@naver.com>
2021-03-02 21:49:54 +0900

    Add: 캔버스 원그리기 추가

--- martian/public/src/main.js
+++ martian/public/src/main.js
@@ -1,1 +1,10 @@
-const a = 1;
+import _ from './util.js';
+
+const canvas = _.$('._canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.strokeStyle = '#000';
+ctx.arc(400, 400, 300, 0, Math.PI * 2);
+ctx.stroke();
+ctx.closePath();

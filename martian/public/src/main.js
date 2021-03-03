9cf7334ec6284ba41128caafcf97aed12c0eab8e
jeonyeonkyu <ehb6915@naver.com>
2021-03-03 21:55:20 +0900

    Add: 16진수 번역하는 함수 추가, 16진수로 번역하는 함수 추가

--- martian/public/src/main.js
+++ martian/public/src/main.js
@@ -1,63 +1,67 @@
 import _ from './util.js';
+import renderPlate from './renderPlate.js';
 
-const plateStuff = {
-  x: 400,
-  y: 400,
-  radius: 300,
-  startAngle: Math.PI * 1.5,
-  endAngle: Math.PI * 2,
-  anticlockwise: false
-}
-
-const Hexadecimal = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-
-const addText = (context, category, midAngle) => {
-  context.textAlign = "center";
-  context.textBaseline = "middle";
-  context.font = "24pt Century Gothic Bold";
-  context.fillStyle = "#fff";
-  context.fillText(category, 400 + Math.cos(midAngle) * 260, 400 + Math.sin(midAngle) * 260);
-}
-
-const numberPlate = ({ context, plateStuff }) => {
-  const { x, y, radius, startAngle, endAngle, anticlockwise } = plateStuff;
-  let angle = startAngle;
-  Hexadecimal.forEach((hex, i) => {
-    const nextAngle = angle + endAngle / Hexadecimal.length;
-    context.beginPath();
-    context.strokeStyle = '#000';
-    context.fillStyle = '#F77269';
-    context.moveTo(x, x);
-    context.arc(x, y, radius, angle, nextAngle, anticlockwise);
-    context.lineTo(x, x);
-    context.fill();
-    context.stroke();
-    const midAngle = (angle + nextAngle) / 2;
-    addText(context, hex, midAngle);
-    context.closePath();
-    angle = nextAngle;
-  })
-}
-
-const arrowPlate = ({ context, plateStuff }) => {
-  const { x, y, radius, endAngle, anticlockwise } = plateStuff;
-  context.beginPath();
-  context.arc(x, y, radius / 3, 0, endAngle, anticlockwise);
-  context.fill();
-  context.stroke();
-  context.closePath();
-}
-
-
-const renderPlate = ({ context, plateStuff }) => {
-  numberPlate({ context, plateStuff });
-  arrowPlate({ context, plateStuff });
-}
-
-
+const Hexadecimal = ['0', '1', '2', '3', '4', '5', '6', '7', '8',
+  '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 
 const $canvas = _.$('._canvas');
 const context = $canvas.getContext('2d');
-renderPlate({ context, plateStuff });
-//'Z'.charCodeAt().toString(16)
-// String.fromCharCode(parseInt('5a',16))
+renderPlate({ context, Hexadecimal });
+
+
+const convertStringToHex = (string) => {
+  return string.charCodeAt().toString(16);
+}
+
+const convertHexToString = (hex) => {
+  return String.fromCharCode(parseInt(hex, 16));
+}
+
+const convertFromMessage = (massage) => {
+
+}
+
+
+const $receiveInput = _.$('._receive_input');
+const $receiveButton = _.$('._receive_button');
+const $receiveInterpreting = _.$('._receive_interpreting');
+
+$receiveButton.addEventListener('click', () => {
+  const convertString = $receiveInput.value.split(' ')
+    .map(e => convertHexToString(e)).join('');
+  $receiveInterpreting.innerText = convertString;
+});
+
+
+const $sendInput = _.$('._send_input');
+const $sendButton = _.$('._send_button');
+const $sendInterpreting = _.$('._send_interpreting');
+
+$sendInput.addEventListener('input', ({ target: { value } }) => {
+  const convertNumber = value.split('')
+    .map(e => convertStringToHex(e))
+    .join(' ');
+  $sendInterpreting.innerText = convertNumber;
+});
+
+
+const message = _.$('._message');
+const messageArray = message.innerText.split(' ');
+const currentMessage = messageArray.shift();
+let prevIndex = 0;
+const currentIndex = Hexadecimal.indexOf(currentMessage[0].toUpperCase());
+const distance = prevIndex - currentIndex;
+const way = distance <= 8 || distance >= -8;
+const step = Math.abs(distance) > 8 ? 16 - Math.abs(distance) : Math.abs(distance);
+
+const prevAngle = _.$('.arrow').style.transform.slice(7).slice(0, -4);
+if (way) {
+  _.$('.arrow').style.transform = `rotate(${Number(prevAngle) + step * 22.5}deg)`;
+} else {
+  _.$('.arrow').style.transform = `rotate(${Number(prevAngle) - step * 22.5}deg)`;
+}
+//구현중..
+
+//11.25
+//22.5
+//68 65 6c 6c 6f 20 65 61 72 74 68 21

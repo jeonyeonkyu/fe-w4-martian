1341e4b2fc279deec1b469a6f82cd6f900277a54
jeonyeonkyu <ehb6915@naver.com>
2021-03-05 15:58:52 +0900

    Refactor: 함수 모듈화 리팩토링

--- martian/public/src/main.js
+++ martian/public/src/main.js
@@ -1,85 +1,44 @@
 import _ from './util.js';
 import renderPlate from './renderPlate.js';
+import runTransceiver from './transceiver.js';
+import {
+  receiveButtonClickHandler, sendInputHandler,
+  sendButtonClickHandler, earthSendButtonClickHandler
+} from './eventHandler.js';
 
 const Hexadecimal = ['0', '1', '2', '3', '4', '5', '6', '7', '8',
   '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 
-const $canvas = _.$('._canvas');
-const context = $canvas.getContext('2d');
+const elements = {
+  $canvas: _.$('._canvas'),
+  $receiveInput: _.$('._receive_input'),
+  $receiveButton: _.$('._receive_button'),
+  $receiveInterpreting: _.$('._receive_interpreting'),
+  $sendInput: _.$('._send_input'),
+  $sendButton: _.$('._send_button'),
+  $sendInterpreting: _.$('._send_interpreting'),
+  $appointed: _.$All('.appointed > div'),
+  $mars: _.$('.mars'),
+  $arrow: _.$('.arrow'),
+  $message: _.$('._message'),
+  $earthReceiveInput: _.$('._earth_receive_input'),
+  $earthSendInput: _.$('._earth_send_input'),
+  $earthSendButton: _.$('._earth_send_button')
+}
+
+const context = elements.$canvas.getContext('2d');
 renderPlate({ context, Hexadecimal });
 
+elements.$receiveButton.addEventListener('click', () => receiveButtonClickHandler(elements));
+elements.$sendInput.addEventListener('input', (event) => sendInputHandler(event, elements));
+elements.$sendButton.addEventListener('click', () => sendButtonClickHandler(elements));
+elements.$earthSendButton.addEventListener('click', () => earthSendButtonClickHandler(elements));
 
-const convertStringToHex = (string) => {
-  return string.charCodeAt().toString(16);
-}
-
-const convertHexToString = (hex) => {
-  return String.fromCharCode(parseInt(hex, 16));
-}
-
-const convertFromMessage = (massage) => {
-
-}
-
-
-const $receiveInput = _.$('._receive_input');
-const $receiveButton = _.$('._receive_button');
-const $receiveInterpreting = _.$('._receive_interpreting');
-
-$receiveButton.addEventListener('click', () => {
-  const convertString = $receiveInput.value.split(' ')
-    .map(e => convertHexToString(e)).join('');
-  $receiveInterpreting.innerText = convertString;
+const observer = new MutationObserver((mutations) => {
+  const communicator = mutations[0].attributeName === 'data-from-mars' ? 'fromMars' : 'fromEarth';
+  runTransceiver(elements, communicator, Hexadecimal);
 });
 
+const config = { attributes: true };
 
-const $sendInput = _.$('._send_input');
-const $sendButton = _.$('._send_button');
-const $sendInterpreting = _.$('._send_interpreting');
-
-$sendInput.addEventListener('input', ({ target: { value } }) => {
-  const convertNumber = value.split('')
-    .map(e => convertStringToHex(e))
-    .join(' ');
-  $sendInterpreting.innerText = convertNumber;
-});
-
-const appointed = _.$All('.appointed > div');
-const addBlinkColor = (index) => {
-  appointed[index].classList.add('blinking');
-}
-
-const removeBlinkColor = (index) => {
-  appointed[index].classList.remove('blinking');
-}
-
-const runTransceiver = ({ $arrow, $message, $receiveInput, Hexadecimal }) => {
-  const messageArray = $message.innerText.split(' ');
-  const size = 360 / Hexadecimal.length;
-  let prevIndex = 0;
-  const allMessageTimer = setInterval(() => {
-    const currentMessage = messageArray.shift().split('');
-    const currentMessageTimer = setInterval(() => {
-      const currentIndex = Hexadecimal.indexOf(currentMessage.shift().toUpperCase());
-      removeBlinkColor(prevIndex);
-      addBlinkColor(currentIndex);
-      const distance = prevIndex - currentIndex;
-      const way = distance >= 8 || (distance <= 0 && distance >= -8);
-      const step = Math.abs(distance) > 8 ? 16 - Math.abs(distance) : Math.abs(distance);
-      const prevAngle = Number($arrow.style.transform.slice(7).slice(0, -4));
-      $arrow.style.transform = way ? `rotate(${prevAngle + step * size}deg)`
-        : `rotate(${prevAngle - step * size}deg)`;
-      prevIndex = currentIndex;
-
-      $receiveInput.value += Hexadecimal[currentIndex];
-      if (!currentMessage[0]) clearInterval(currentMessageTimer);
-    }, 2000);
-    $receiveInput.value += ' ';
-    if (!messageArray[0]) clearInterval(allMessageTimer);
-  }, 5000);
-}
-
-const $arrow = _.$('.arrow');
-const $message = _.$('._message');
-
-runTransceiver({ $arrow, $message, $receiveInput, Hexadecimal });
+observer.observe(elements.$message, config);

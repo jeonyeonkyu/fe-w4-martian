c3f7f75701907637dc0f8725fc79c33299fddba8
jeonyeonkyu <ehb6915@naver.com>
2021-03-06 18:22:22 +0900

    Feat: MyPromise step2 구현

--- MyPromise2.js
+++ MyPromise2.js
@@ -1,28 +1,23 @@
 // 내가 만든 Promise
 class MyPromise {
   constructor(callback) {
-    callback(this.resolve, this.reject);
+    this.cbList = [];
+    callback(this.resolve.bind(this));
   }
 
   then(callback) {
+    this.cbList.push(callback);
     return this;
   }
 
   resolve(data) {
-
+    setTimeout(() => this.cbList.reduce((acc, cur) => cur(acc), data), 0);
   }
-
-  reject(data) {
-
-  }
-}
-
-const callback = (resolve, reject) => {
-  setTimeout(() => resolve('completed'), 1000);
 }
 
 //MyPromise 를 사용하기
-const p = new MyPromise(callback);
-p.then((res) => console.log(res));
+const p = new MyPromise((resolve) => {
+  setTimeout(() => resolve('completed'), 1000);
+});
 
-console.log(p.data);
+p.then((res) => console.log(res));
